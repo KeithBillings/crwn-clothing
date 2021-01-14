bca2a5992637eaf60133520a7feed515f357ef2f
Keith Billings <heyitsbo91@gmail.com>
2021-01-13 22:41:22 -0700

    use shop data to populate new collection preview components

--- src/pages/shop/shop.jsx
+++ src/pages/shop/shop.jsx
@@ -1,10 +1,20 @@
 import React, { useState } from "react";
-import SHOP_DATA from "../../utils/shop.data";
 // import { withRouter } from "react-router-dom";
+
+// Components
+import SHOP_DATA from "../../utils/shop.data";
+import CollectionPreview from "../../components/collection-preview/collection-preview.component";
 
 function ShopPage() {
   const [shopData, setShopData] = useState(SHOP_DATA);
-  return <div>SHOP PAGE</div>;
+
+  return (
+    <div className="shop-page">
+      {shopData.map(({ id, ...collecitonProps }) => {
+        return <CollectionPreview key={id} {...collecitonProps} />;
+      })}
+    </div>
+  );
 }
 
 export default ShopPage;
